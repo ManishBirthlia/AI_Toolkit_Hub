216841b8ca80471db7d61f61ebf2154c306b8307
Manish Birthlia <manishbirthliya@gmail.com>
2026-04-07 19:46:48 +0530

    test: add integration test for Bark TTS synthesis functionality

--- tests/TTS/test_bark.py
+++ tests/TTS/test_bark.py
@@ -1,6 +1,11 @@
 import os
+import sys
 import asyncio
 import time
+
+# Add the project root to the Python path so it can find "modules" and "utils"
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from modules.TTS.Bark import BarkTTS
 from utils.logger import get_logger
 
